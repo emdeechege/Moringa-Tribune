66388cea799d8b903b72ead4ccc3c1ab70cdf230
Tom Chege <emdeechege@gmail.com>
2018-09-24 16:22:16 +0300

    added tags to link many to many relationship, tags,editor,articles

--- news/models.py
+++ news/models.py
@@ -10,3 +10,16 @@ class Editor(models.Model):
         return self.first_name
     class Meta:
         ordering = ['first_name']
+
+class tags(models.Model):
+    name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+    title = models.CharField(max_length =60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor)
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add=True)
