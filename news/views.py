acc35ba43228c4c2b5da5b839f0ed4aeb8a99302
Tom Chege <emdeechege@gmail.com>
2018-09-25 15:17:38 +0300

    forms added for subscribing

--- news/views.py
+++ news/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.http  import HttpResponse, Http404
 import datetime as dt
 from .models import *
+from .forms import NewsLetterForm
 # Create your views here.
 def welcome(request):
     return render(request,'welcome.html')
@@ -9,7 +10,14 @@ def welcome(request):
 def news_of_day(request):
     date = dt.date.today()
     news = Article.todays_news()
-    return render(request, 'all-news/today-news.html', {"date": date,"news":news})
+
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            print('valid')
+    else:
+        form = NewsLetterForm()
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news,"letterForm":form})
 
 
 # def convert_dates(dates):
